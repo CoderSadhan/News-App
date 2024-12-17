5ee6995902cf7842e7c3d3d2657b11bb2b08e90b
Sadhan Das <113288651+CoderSadhan@users.noreply.github.com>
2024-12-18 02:17:40 +0530

    .

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = ({ setCategory }) => {
   return (
     <nav className="navbar navbar-expand-lg  nav-top" >
       <div className="container-fluid">
-        <a className="navbar-brand" href="#"><span className="badge bg-light text-dark fs-3">News-Dunia</span></a>
+        <div className="navbar-brand"><span className="badge bg-light text-dark fs-3">News-Dunia</span></div>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
